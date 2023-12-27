c82ff3679b552ebfa6b094c0ec8c2f5843edf69b
MZT7 <40639877+MZT7@users.noreply.github.com>
2023-12-27 09:35:19 +0100

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "@/components/normalC/Provider";
+import { Providers } from "@/components/normalComponent/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
